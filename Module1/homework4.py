032dd1efeeb001809588a46bacc52cd4ea4659ef
Qrasann <rnabirin92@gmail.com>
2024-04-25 10:17:31 +0300

    1

---
+++ Module1/homework4.py
@@ -0,0 +1,8 @@
+immutable_var = 1, 2, 3, True, 'minecraft'
+print(immutable_var)
+immutable_var[4] = 'Roblox'
+print(immutable_var)
+#Python не позволяет изменять значения элементов кортежа после его создания.
+mutable_list = [4, 5, 6, False, 'Наруто']
+mutable_list[4] = "Саске"
+print(mutable_list)
