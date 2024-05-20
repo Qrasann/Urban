18a8bc74b7c622cd4a365354b49c7003ce5ae062
Qrasann <rnabirin92@gmail.com>
2024-05-20 18:02:50 +0300

    push

--- Module2/homework_for.py
+++ Module2/homework_for.py
@@ -2,5 +2,5 @@ cars = ['BMW', 'MB', 'LADA', 'KIA', 'HONDA']
 cars_count = 0
 for car in cars:
     cars_count += 10
-    print('Я езжу на машине ', car, cars_count)
-
+    print('Я езжу на машине ', car)
+    print(f'Значение переменой cars_count:{cars_count}\n')
