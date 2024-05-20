4fad1627a8ab1fc35878fff7e95bf33a4c5279b6
Qrasann <rnabirin92@gmail.com>
2024-05-20 14:47:14 +0300

    push

---
+++ Module1/variables/homework2.py
@@ -0,0 +1,5 @@
+completed_homeworks = int(12)
+work_time = float(1.5)
+course_name = 'Python'
+time_one_homework = work_time / completed_homeworks
+print(f'{course_name}, всего задач: {completed_homeworks}, затрачено часов: {work_time}, среднее время выполнения {time_one_homework}.')
