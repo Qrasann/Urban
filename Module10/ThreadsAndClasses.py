d012b96651f774865559d4ebf8e18fc4a5d08dfb
RomaQrasan <rnabirin92@gmail.com>
2024-07-21 07:19:40 +0300

    /

--- Module10/ThreadsAndClasses.py
+++ Module10/ThreadsAndClasses.py
@@ -3,31 +3,26 @@ import time
 
 
 class Knight(threading.Thread):
-  enemies = 100
-  lock = threading.Lock()
-
   def __init__(self, name, power):
     super().__init__()
     self.name = name
     self.power = power
+    self.enemies = 100
 
   def run(self):
     days = 0
     print(f"{self.name}, на нас напали!")
-    while True:
-      with Knight.lock:
-        if Knight.enemies <= 0:
-          break
-        Knight.enemies -= self.power
-        days += 1
-        remaining_enemies = max(Knight.enemies, 0)
-        print(f"{self.name}, сражается {days} день(дня)..., осталось {remaining_enemies} воинов.")
+    while self.enemies > 0:
+      days += 1
+      self.enemies -= self.power
+      remaining_enemies = max(self.enemies, 0)
+      print(f"{self.name} сражается {days} день(дня)..., осталось {remaining_enemies} воинов.")
       time.sleep(1)
     print(f"{self.name} одержал победу спустя {days} дней(дня)!")
 
 
 first_knight = Knight('Sir Naruto', 7)
-second_knight = Knight('Sir Steve', 3)
+second_knight = Knight('Sir Steve', 10)
 
 first_knight.start()
 second_knight.start()
