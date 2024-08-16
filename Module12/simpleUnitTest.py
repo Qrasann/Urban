746bd7a88b2f5303970e0e227c269d4750bcb6be
qrasann <rnabirin92@gmail.com>
2024-08-16 15:23:56 +0300

    /

--- Module12/simpleUnitTest.py
+++ Module12/simpleUnitTest.py
@@ -1,19 +1,31 @@
-from runner import Runner
 import unittest
+from runner import Runner
 
 class RunnerTest(unittest.TestCase):
+    is_frozen = False
+
+    def freeze_check(func):
+        def wrapper(self, *args, **kwargs):
+            if self.is_frozen:
+                self.skipTest('Тесты в этом кейсе заморожены')
+            return func(self, *args, **kwargs)
+        return wrapper
+
+    @freeze_check
     def test_walk(self):
         runner = Runner('TestRunner')
         for _ in range(10):
             runner.walk()
         self.assertEqual(runner.distance, 50)
 
+    @freeze_check
     def test_run(self):
         runner = Runner('TestRunner')
         for _ in range(10):
             runner.run()
         self.assertEqual(runner.distance, 100)
 
+    @freeze_check
     def test_challenge(self):
         runner1 = Runner('TestRunner1')
         runner2 = Runner('TestRunner2')
@@ -23,8 +35,7 @@ class RunnerTest(unittest.TestCase):
         for _ in range(3):
             runner2.run()
             runner2.walk()
-        self.assertNotEqual(runner1.distance, runner2.distance )
+        self.assertNotEqual(runner1.distance, runner2.distance)
 
 if __name__ == '__main__':
     unittest.main()
-
--- Module12/testMetods.py
+++ Module12/testMetods.py
@@ -2,22 +2,16 @@ import unittest
 from runner_and_tournament import Runner, Tournament
 
 class TournamentTest(unittest.TestCase):
-    all_results = {}
+    is_frozen = True
 
-    @classmethod
-    def setUpClass(cls):
-        cls.all_results = {}
+    def freeze_check(func):
+        def wrapper(self, *args, **kwargs):
+            if self.is_frozen:
+                self.skipTest('Тесты в этом кейсе заморожены')
+            return func(self, *args, **kwargs)
+        return wrapper
 
-    def setUp(self):
-        self.runner1 = Runner('Usain', speed=10)
-        self.runner2 = Runner('Andrei', speed=9)
-        self.runner3 = Runner('Nick', speed=3)
-
-    @classmethod
-    def tearDownClass(cls):
-        for result in cls.all_results.values():
-            print(result)
-
+    @freeze_check
     def test_race_usain_nick(self):
         tournament = Tournament(90, self.runner1, self.runner3)
         results = tournament.start()
@@ -25,6 +19,7 @@ class TournamentTest(unittest.TestCase):
         last_place = max(self.all_results)
         self.assertTrue(self.all_results[last_place] == 'Nick')
 
+    @freeze_check
     def test_race_andrei_nick(self):
         tournament = Tournament(90, self.runner2, self.runner3)
         results = tournament.start()
@@ -32,6 +27,7 @@ class TournamentTest(unittest.TestCase):
         last_place = max(self.all_results)
         self.assertTrue(self.all_results[last_place] == 'Nick')
 
+    @freeze_check
     def test_race_usain_andrei_nick(self):
         tournament = Tournament(90, self.runner1, self.runner2, self.runner3)
         results = tournament.start()
