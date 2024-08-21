ca06e2e444e3b6dbe3eb9aad62447a7a060eba69
Qrasann <roma.nabirin@mail.ru>
2024-08-21 10:22:32 +0300

    \

--- Module13/bot.py
+++ Module13/bot.py
@@ -1,15 +1,26 @@
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters import Command
-from aiogram.utils import run_polling
-from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
 
-api = "7058673152:AAFnRsfr2X0MeuHJAxIev_bxUWcKy60-ck0"
+api = ""
 bot = Bot(token=api)
-dp = Dispatcher(storage=MemoryStorage())
+dp = Dispatcher(bot, storage=MemoryStorage())
 
-@dp.message(Command('start'))
-async def send_welcome(message: types.Message):
-    await message.answer("Привет!")
+
+@dp.message_handler(text=['Urban', 'ff'])
+async def urban_message(message):
+  print("Urban message")
+
+
+@dp.message_handler()
+async def all_message(message):
+  print("Введите команду /start, чтобы начать общение.")
+
+
+@dp.message_handler()
+async def start(message):
+  print("Привет! Я бот помогающий твоему здоровью.")
+
 
 if __name__ == '__main__':
-    run_polling(dp, skip_updates=True)
+  executor.start_polling(dp, skip_updates=True)
