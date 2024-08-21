9aad8a0a65867577f4af31851f89dab0e6d277e9
RomaQrasan <68272489+Qrasann@users.noreply.github.com>
2024-08-21 16:00:59 +0300

    bot.py

--- Module13/bot.py
+++ Module13/bot.py
@@ -7,13 +7,22 @@ bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
 
+@dp.message_handler(text=['Urban', 'ff'])
+async def urban_message(message):
+  print("Urban message")
+
+
 @dp.message_handler(text=['/start', 'start'])
 async def start(message):
   print("Привет! Я бот помогающий твоему здоровью.")
 
+@dp.message_handler()
+async def massages(message):
+  print("Мы получили сообщение!")
+
 
 @dp.message_handler()
-async def all_message(message):
+async def all_massages(message):
   print("Введите команду /start, чтобы начать общение.")
 
 
