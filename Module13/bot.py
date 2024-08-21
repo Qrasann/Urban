66511213a064d71e5d09e660db5570285f918ea9
Qrasann <roma.nabirin@mail.ru>
2024-08-21 10:27:34 +0300

    \

--- Module13/bot.py
+++ Module13/bot.py
@@ -2,24 +2,19 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
 
-api = ""
+api = "7397323719:AAGK4BQLwKqQ3kK9H0gYxRo8-IRA9sMCs2U"
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
 
-@dp.message_handler(text=['Urban', 'ff'])
-async def urban_message(message):
-  print("Urban message")
+@dp.message_handler(text=['/start', 'start'])
+async def start(message):
+  print("Привет! Я бот помогающий твоему здоровью.")
 
 
 @dp.message_handler()
 async def all_message(message):
   print("Введите команду /start, чтобы начать общение.")
-
-
-@dp.message_handler()
-async def start(message):
-  print("Привет! Я бот помогающий твоему здоровью.")
 
 
 if __name__ == '__main__':
