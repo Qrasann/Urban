12ae1467c99442eb9bc449e5ce27e520356fd6e0
qrasann <rnabirin92@gmail.com>
2024-08-29 17:17:19 +0300

    /

--- Module13/bot.py
+++ Module13/bot.py
@@ -3,6 +3,7 @@ from aiogram.filters import Command
 from aiogram.utils import run_polling
 from aiogram.fsm.storage.memory import MemoryStorage
 
+
 api = "7058673152:AAFnRsfr2X0MeuHJAxIev_bxUWcKy60-ck0"
 bot = Bot(token=api)
 dp = Dispatcher(storage=MemoryStorage())
--- Module13/bot3.py
+++ Module13/bot3.py
@@ -3,12 +3,35 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher import FSMContext
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import asyncio
 
-api = ""
+api = ''
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
+
+kb = InlineKeyboardMarkup()
+button = InlineKeyboardButton(text = 'Информация', callback_data='info')
+kb.add(button)
+
+start_menu = ReplyKeyboardMarkup(
+  keyword=[
+    [KeyboardButton(text='Info')],
+    [
+      KeyboardButton(text='shop'),
+      KeyboardButton(text='donate')
+    ]
+  ], resize_keyboard= True
+)
+@dp.message_handler(commands=['start'])
+async def starter(message):
+  await message.answer('Рады вас видеть!', reply_markup = start_menu)
+
+#@dp.callback_query_handler(text = 'info')
+#async def infor(call):
+  #await call.message.anwer('Информация о боте')
+  #await call.answer()
+
 
 kb = ReplyKeyboardMarkup(resize_keyboard=True)
 button = KeyboardButton(text = 'Рассчитать')
