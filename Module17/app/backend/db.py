a7c128cac561e1cb7d15b1e16fe787b153f294a1
qrasann <rnabirin92@gmail.com>
2024-10-10 18:02:21 +0300

    \

--- Module17/app/backend/db.py
+++ Module17/app/backend/db.py
@@ -1,10 +1,8 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///taskmanager.db'
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
-
+database_url = 'sqlite:///taskmanager.db'
+engine = create_engine(database_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
