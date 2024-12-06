00e555983faf4164ad45c5b52d91d3c5e3da114e
RomaQrasan <68272489+Qrasann@users.noreply.github.com>
2024-12-06 13:05:36 +0300

    Delete Module4 directory

--- Module4/ModulesAndPackets/main.py
+++
@@ -1,4 +0,0 @@
-from test import hello_world1, hello_world2
-
-hello_world1()
-hello_world2()
--- Module4/ModulesAndPackets/test.py
+++
@@ -1,5 +0,0 @@
-def hello_world1():
-  print("Hello, world!")
-
-def hello_world2():
-  print("Hello, world!\n")
